553bc156b9a56f56bfe1545e02d9d19a9b932f12
puresox <puresox@163.com>
2020-07-17 16:29:01 +0800

    1.1.5

--- pcrFarm.py
+++ pcrFarm.py
@@ -9,7 +9,7 @@ from mutiprocessFunc import worker
 if __name__ == "__main__":
     # pyinstaller: When using the multiprocessing module, you must call
     multiprocessing.freeze_support()
-    version = "1.1.0"
+    version = "1.1.5"
     print("当前版本：%s" % (version))
 
     """获取设备列表"""
